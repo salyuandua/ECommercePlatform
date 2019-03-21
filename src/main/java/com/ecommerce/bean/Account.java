d8a928ee25a7be2dac072c0e22896fe61f0b6050
Li Xuejian <l18765827739@gmail.com>
2019-03-21 10:51:33 -0400

    First Sub

---
+++ src/main/java/com/ecommerce/bean/Account.java
@@ -0,0 +1,16 @@
+package com.ecommerce.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="accounts")
+public class Account {
+	
+	private int id;
+	
+	
+	
+	
+
+}
